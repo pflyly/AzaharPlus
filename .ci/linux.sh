41e78aff00ea494ca8c7c90b8de6ceb10c8c7286
Escary <81011361+pflyly@users.noreply.github.com>
2025-04-20 00:04:42 +0100

    Update linux.sh

--- .ci/linux.sh
+++ .ci/linux.sh
@@ -18,8 +18,6 @@ if [ "$GITHUB_REF_TYPE" == "tag" ]; then
 	export EXTRA_CMAKE_FLAGS=($EXTRA_CMAKE_FLAGS -DENABLE_QT_UPDATE_CHECKER=ON)
 fi
 
-
-mkdir build
 cd build
 cmake .. -G Ninja \
     -DCMAKE_BUILD_TYPE=Release \
