fc6b460c0335f7c7daef184443d945c4cc2f8d52
Escary <81011361+pflyly@users.noreply.github.com>
2025-04-21 14:50:16 +0100

    Update linux.sh

--- .ci/linux.sh
+++ .ci/linux.sh
@@ -35,12 +35,10 @@ if [ "$TARGET" = "appimage" ]; then
     
     # Use uruntime to generate dwarfs appimage
     rm -f ./bundle/*.AppImage
-    wget -q "https://github.com/VHSgunzo/uruntime/releases/download/v0.3.4/uruntime-appimage-dwarfs-x86_64" -O ./uruntime 
+    wget -q "https://github.com/VHSgunzo/uruntime/releases/download/v0.3.6/uruntime-appimage-dwarfs-x86_64" -O ./uruntime 
     chmod a+x ./uruntime
     ./uruntime --appimage-mkdwarfs -f --set-owner 0 --set-group 0 --no-history --no-create-timestamp \
     --compression zstd:level=22 -S26 -B32 --header ./uruntime -i ./AppDir-azahar -o azahar.AppImage
-    ./uruntime --appimage-mkdwarfs -f --set-owner 0 --set-group 0 --no-history --no-create-timestamp \
-    --compression zstd:level=22 -S26 -B32 --header ./uruntime -i ./AppDir-azahar-room -o azahar-room.AppImage
     mv ./*.AppImage ./bundle
     
     ccache -s
